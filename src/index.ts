64c4180d5fd2e3b5924cdc21a448dc7a49432641
Emily Wang <egw7501@nyu.edu>
2024-07-07 17:16:10 -0500

    changed http to https

--- src/index.ts
+++ src/index.ts
@@ -115,7 +115,7 @@ app.post('/editWithPicture', upload.single('file'), async (req, res) => {
         });
     }
     else {
-        res.redirect('http://devhunt-server-e141054a4ce8.herokuapp/editNoPicture')
+        res.redirect('https://devhunt-server-e141054a4ce8.herokuapp/editNoPicture')
     }
 })
 
