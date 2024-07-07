a6e1d5252f056211b8cd04c444c8d9da9bd1ca90
Emily Wang <egw7501@nyu.edu>
2024-07-07 18:58:02 -0500

    updated cors origin

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session())
 app.use(cors({
-    origin: 'https://devhunt-client-564fb3b4c40a.herokuapp.com/',
+    origin: 'https://devhunt-client-564fb3b4c40a.herokuapp.com',
     credentials: true,
     methods: 'GET,POST,PUT,DELETE',
 }))
